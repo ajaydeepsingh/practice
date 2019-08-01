4b8d7a6a95e9e0d9fee7d2d1b4fe99f548bcf750
Ajay Singh <singhajay08@gmail.com>
2019-08-01 00:55:37 -0400

    add explanation

--- codeSignal/hasPathWithGivenSum.py
+++ codeSignal/hasPathWithGivenSum.py
@@ -1,7 +1,7 @@
-#
-# Given a binary tree t and an integer s, determine whether there is a root to 
-# leaf path in t such that the sum of vertex values equals s.
-
+"""
+Given a binary tree t and an integer s, determine whether there is a root to 
+leaf path in t such that the sum of vertex values equals s.
+"""
 
 
 # Binary trees are already defined with this interface:
@@ -22,3 +22,19 @@ def hasPathWithGivenSum(t, s):
 
     return hasPathWithGivenSum(t.left, s) or hasPathWithGivenSum(t.right, s)
 
+"""
+Base case
+This question wants to find a path from node to the leaf, so the node who 
+satisfies must be a leaf (not node.left and not nood.right)
+
+I want to recursively subtract the sum by current node's value, so the leaf 
+node of correct path must have the same value of its assigned sum
+
+Recursive step
+We want to traverse all the nodes, so node.left and node.right must be 
+called parallelly
+
+One valid path is enough, so we want to use or to connect two "branches"
+Considering each node as the root of its children, sum should be modified 
+from its parent
+"""
